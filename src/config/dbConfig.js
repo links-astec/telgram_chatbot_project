447dbc5e1fe33782b8887cb59a9ec98d3869c487
Nyong Faal Anthony <nyongfaalanthony@gmail.com>
2024-01-28 08:26:59 +0000

    update 4

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg')
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL_URL + "?sslmode=require",
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 })  
 
 pool.connect((err) => {
